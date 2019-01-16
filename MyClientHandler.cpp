3310b76920d353662c0e92b95e33543d5c8d9a87
EfratHrrison <efratharr@gmail.com>
2019-01-16 20:52:57 +0200

    note done

--- MyClientHandler.cpp
+++ MyClientHandler.cpp
@@ -9,94 +9,141 @@
 #include "string"
 #include "Matrix.h"
 #include <mutex>
+#include <sys/socket.h>
 
 void MyClientHandler::handleClient(int clientSock) {
-    string problem="";
-    string solution="";
+    string problem = "";
+    string solution = "";
     char *whriteBack;
     int clilen;
-    char* chr;
-    int x; int y;
-    int countLines =0;
+    int x;
+    int y;
+    int n;
+    int countLines = 0;
     vector<string> vector1;
     vector<string> vector2;
-    vector<State<Point>*> MatrixV;
+    vector<State<Point> *> MatrixV;
+    char buffer[1024];
 
-    ssize_t n;
-    while(true) {
-        char buffer[1000];
+    if (clientSock < 0) {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
+
+    while (true) {
+        problem = "";
+        MatrixV.clear();
+        vector1.clear();
+        vector2.clear();
+        solution = "";
+
+        string buff = "";
+
         vector<string> vectorBuff;
         // If connection is established then start communicating
-        while(true){
-            bzero(buffer, 1000);
-            n = read(clientSock, buffer, 1000);
-            if (n < 0) {
-                perror("ERROR reading from socket");
-                exit(1);
-            }
-            //if the client entered "end" that means we are done
-            if(strcmp(buffer,"end")==0){
+        while (true) {
+            //bzero(buffer, 256);
+            char buffer[1024];
+            int numBytesRead = recv(clientSock, buffer, sizeof(buffer), 0);
+            if (numBytesRead > 0) {
+                for (int i = 0; i < numBytesRead; ++i) {
+                    char c = buffer[i];
+                    if (c == '\n') {
+                        if (buff.length() > 0) {
+                            printf("Next command is [%s]\n", buff.c_str());
+                            if (buff == "end") {
+                                break;
+                            }
+                            vectorBuff.push_back(buff);
+                            problem += buff;
+                            problem += ".";
+                            buff = "";
+                        }
+                    } else {
+                        buff += c;
+                    }
+                }
+            } else {
+                printf("Socket closed or socket error!\n");
                 break;
             }
-            vectorBuff.push_back(buffer);
-            problem+=buffer;
-            problem+=".";
-        }
-        countLines=vectorBuff.size()-2;
-        //initiail point
-        string s = vectorBuff[countLines];
-        vector1= explode(s,',');
-        x = std::stoi(vector1[0]);
-        y = std::stoi(vector1[1]);
-        Point initialP(x,y);
-
-        //goal point
-        s = vectorBuff[countLines+1];
-        vector1= explode(s,',');
-        x = std::stoi(vector1[0]);
-        y = std::stoi(vector1[1]);
-        Point goalP(x,y);
-
-        int row=countLines;
-        string numOfClo=vectorBuff[0];
-        vector2= explode(numOfClo,',');
-        int col=vector2.size();
-        for(int i=0 ; i< row ; i++) {
-            s = vectorBuff[i];
-            vector1= explode(s,',');
-            for(int j=0 ; j< col; j++) {
-                //put each point in a state point vector to create a searchebla
-                State<Point>* state = new State<Point>(Point(i, j), stod(vector1[j]));
-                MatrixV.push_back(state);
+            if (buff == "end") {
+                buff = "";
+                break;
             }
         }
-        Searchable<Point>* searchableM = new Matrix(MatrixV,this->getStatePoint(MatrixV,initialP),this->getStatePoint(MatrixV,goalP));
+//
+//                bzero(buffer, 1000);
+//            n = read(clientSock, buffer, 1000);
+//            if (n < 0) {
+//                perror("ERROR reading from socket");
+//                exit(1);
+//            }
+//            //if the client entered "end" that means we are done
+//            if(strcmp(buffer,"end")==0){
+//                break;
+//            }
+//            vectorBuff.push_back(buffer);
+//            problem+=buffer;
+//            problem+=".";
+//        }
+            countLines = vectorBuff.size() - 2;
+            //initiail point
+            string s = vectorBuff[countLines];
+            vector1 = explode(s, ',');
+            x = std::stoi(vector1[0]);
+            y = std::stoi(vector1[1]);
+            Point initialP(x, y);
 
-        //if we dond have the problem already
-        //solve the [roblem and save it in our cach manager
-        if(!this->cacheManager->haveSolution(problem)) {
-            solution = this->solver->solve(searchableM);
-            this->cacheManager->updateSolutions(problem,solution);
-            this->cacheManager->addSolution(problem,solution);
-        }
-            //we already have the solution for this problem
-        else{
-            solution = this->cacheManager->getSolution(problem);
-        }
-        whriteBack = const_cast<char *>(solution.c_str());
-        //printf("Here is the message after: %s\n", whriteBack);
-        n = write(clientSock, whriteBack, strlen(whriteBack));
-        if (n < 0) {
-            perror("ERROR writing to socket");
-            exit(1);
-        }
+            //goal point
+            s = vectorBuff[countLines + 1];
+            vector1 = explode(s, ',');
+            x = std::stoi(vector1[0]);
+            y = std::stoi(vector1[1]);
+            Point goalP(x, y);
 
-        for (State<Point> *state : searchableM->getSearchable()) {
-            delete (state);
+            int row = countLines;
+            string numOfClo = vectorBuff[0];
+            vector2 = explode(numOfClo, ',');
+            int col = vector2.size();
+            for (int i = 0; i < row; i++) {
+                s = vectorBuff[i];
+                vector1 = explode(s, ',');
+                for (int j = 0; j < col; j++) {
+                    //put each point in a state point vector to create a searchebla
+                    State<Point> *state = new State<Point>(Point(i, j), stod(vector1[j]));
+                    MatrixV.push_back(state);
+                }
+            }
+            Searchable<Point> *searchableM = new Matrix(MatrixV, this->getStatePoint(MatrixV, initialP),
+                                                        this->getStatePoint(MatrixV, goalP));
+
+            //if we dond have the problem already
+            //solve the [roblem and save it in our cach manager
+            if (!this->cacheManager->haveSolution(problem)) {
+                solution = this->solver->solve(searchableM);
+                this->cacheManager->updateSolutions(problem, solution);
+                this->cacheManager->addSolution(problem, solution);
+            }
+                //we already have the solution for this problem
+            else {
+                solution = this->cacheManager->getSolution(problem);
+            }
+            whriteBack = const_cast<char *>(solution.c_str());
+            //printf("Here is the message after: %s\n", whriteBack);
+            n = write(clientSock, whriteBack, strlen(whriteBack));
+            if (n < 0) {
+                perror("ERROR writing to socket");
+                exit(1);
+            }
+
+            for (State<Point> *state : searchableM->getSearchable()) {
+                delete (state);
+            }
+            delete (searchableM);
+            break;
         }
-        delete (searchableM);
-        break;
-    }
 }
 /**
  * this func retuns a spacific state point we need
@@ -104,32 +151,31 @@ void MyClientHandler::handleClient(int clientSock) {
  * @param initial the point we are looking for
  * @return the state point
  */
-State<Point>* MyClientHandler::getStatePoint(vector<State<Point> *> searchable, Point initial) {
-    for (int i = 0; i < searchable.size(); ++i) {
-        Point point(searchable[i]->getState().getX(), searchable[i]->getState().getY());
-        if (point == initial) {
-            return searchable[i];
+    State<Point> *MyClientHandler::getStatePoint(vector<State<Point> *> searchable, Point initial) {
+        for (int i = 0; i < searchable.size(); ++i) {
+            Point point(searchable[i]->getState().getX(), searchable[i]->getState().getY());
+            if (point == initial) {
+                return searchable[i];
+            }
         }
     }
-}
 /**
  * this function split each line by a spacific char
  * @param s our string - line
  * @param c the char we are spliting according to him
  * @return vector of strings after we splited the string
  */
-vector<string> MyClientHandler::explode(string& s, const char& c) {
-    string buff{""};
-    vector<string> v;
-    for(auto n:s) {
-        if(n != c) {
-            buff += n;
+    vector<string> MyClientHandler::explode(string &s, const char &c) {
+        string buff{""};
+        vector<string> v;
+        for (auto n:s) {
+            if (n != c) {
+                buff += n;
+            } else {
+                v.push_back(buff);
+                buff = "";
+            }
         }
-        else {
-            v.push_back(buff);
-            buff="";
-        }
+        v.push_back(buff);
+        return v;
     }
-    v.push_back(buff);
-    return v;
-}
